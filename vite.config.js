d2d639cc508826c9cd15914cb49820b9bb476a87
sfh525 <salmanfaizhidayat525@gmail.com>
2025-10-04 08:44:44 +0700

    attempt 1 to fix white page issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Use relative paths for GitHub Pages
   build: {
     outDir: 'dist',
     emptyOutDir: true
